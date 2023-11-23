d054b0cf2c69367df5c9e60b42bc0159f44fda72
Sophie Blanchon <sblanchon@gmail.com>
2023-11-23 10:08:55 +0100

    yarn cmd & cos

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const cors = require('cors');
+app.use(cors());s
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
